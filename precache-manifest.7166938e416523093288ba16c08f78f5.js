1b187d4810a183208b98ee9797cb90d3e4814130
Andrius Svilys <iciukas@gmail.com>
2020-09-21 22:03:34 +0300

    Updates

---
+++ precache-manifest.7166938e416523093288ba16c08f78f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd42ce0dd3645c00496d70240d69da37",
+    "url": "/carousel/index.html"
+  },
+  {
+    "revision": "4b3ce547a9092f3948f9",
+    "url": "/carousel/static/css/main.c0a4cb52.chunk.css"
+  },
+  {
+    "revision": "6289c48578061aab7dd6",
+    "url": "/carousel/static/js/2.de3a3b04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carousel/static/js/2.de3a3b04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b3ce547a9092f3948f9",
+    "url": "/carousel/static/js/main.90629e57.chunk.js"
+  },
+  {
+    "revision": "2334337016e041c9c1cf",
+    "url": "/carousel/static/js/runtime-main.9b8b9a58.js"
+  }
+]);
